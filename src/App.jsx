321c4393870dd63610c04a8e9be3f5437354698c
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2023-12-09 18:51:15 -0600

    Created register module and verifies if a user is logged in to hide opts

--- src/App.jsx
+++ src/App.jsx
@@ -44,35 +44,43 @@ function App() {
   return (
     <div className="container d-flex flex-wrap justify-content-center align-items-center min-vh-100">
 
-        <div className="col-12 col-md-4 ms-0">
-          {/* When clicked, opens login modal */}
-          <div className="card text-center h-100 cardHover" onClick={openModal}>
-            <div className="card-body">
-              <h5 className="card-title card-header">Log In</h5>
-              <div className="card-text mt-1">
-                <p>Log in here!</p>
-                <i className='bi bi-arrow-90deg-down iconSizes'></i>
+        {!userRol ? (
+          <div className="col-12 col-md-4 ms-0">
+            {/* When clicked, opens login modal */}
+            <div className="card text-center h-100 cardHover" onClick={openModal}>
+              <div className="card-body">
+                <h5 className="card-title card-header">Log In</h5>
+                <div className="card-text mt-1">
+                  <p>Log in here!</p>
+                  <i className='bi bi-arrow-90deg-down iconSizes'></i>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          ) : (
+            <div className="col-12 col-md-4 ms-0 h5 text-white text-center">You're logged in!</div>
+          ) 
+        }
 
         {/* Only shows modal if showModal = true and passes function trough props so modal can handle*/}
         {showModal && <LogInModal onClose={closeModal} onLogin={handleUserRol}/>}
 
-        <div className="col-12 col-md-4 ms-1">
-        <div className="card text-center h-100 cardHover" onClick={openRegister}>
-            <div className="card-body">
-              <h5 className="card-title card-header">Register</h5>
-              <div className="card-text mt-1">
-                <p>Register here!</p>
-                <i className='bi bi-check-square-fill iconSizes'></i>
+        {/* *If there is not a logged in user */}
+        {!userRol &&
+          <div className="col-12 col-md-4 ms-1">
+          <div className="card text-center h-100 cardHover" onClick={openRegister}>
+              <div className="card-body">
+                <h5 className="card-title card-header">Register</h5>
+                <div className="card-text mt-1">
+                  <p>Register here!</p>
+                  <i className='bi bi-check-square-fill iconSizes'></i>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
 
-        {opRegModal && <RegisterModal onClose={closeRegister}/>}
+        {opRegModal && <RegisterModal onClose={closeRegister} onLogin={handleUserRol}/>}
 
         {userRol == 1 &&
           <div className="col-12 col-md-4 ms-1">
--- src/components/registerModal.jsx
+++ src/components/registerModal.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
+import axios from 'axios';
+
 //*Gets closeModal function by props
-function RegisterModal({onClose}) {
+function RegisterModal({onClose, onLogin}) {
     //*Use state for login
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [repPassword, setRepPassword] = useState('');
+    const [age, setAge] = useState(0);
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+
     const [errorMsg, setErrorMsg] = useState('');
 
+    //*Methods for handling register data
     const handleUsername = (event)=>{
         setUsername(event.target.value);
     }
@@ -22,14 +29,69 @@ function RegisterModal({onClose}) {
         setRepPassword(event.target.value);
     }
 
-    const handleLogin = ()=>{
-        console.log(username);
-        console.log(password);
+    const handleAge = (event)=>{
+      setAge(event.target.value);
+    }
 
+    const handleName = (event)=>{
+      setName(event.target.value);
+    }
+
+    const handleLastName = (event)=>{
+      setLastName(event.target.value);
+    }
+
+    const handleLogin = async ()=>{
+      try{
+        //*Create json user to send it 
+        const loginUser = {
+          "username": username,
+          "password": password
+        }
+
+        const res = await axios.post('http://localhost:5000/api/users/login/', loginUser);
+        const data = res.data;
+				//*Store data on session storage (only current tab)
+        sessionStorage.clear();
+				sessionStorage.setItem('token', data.token);
+				sessionStorage.setItem('username', data.username);
+				sessionStorage.setItem('rol', data.rol);
+        //*We set the user rol
+        onLogin(data.rol);
+      }catch(error){
+        console.log(error);
+      }
+    }
+
+    //*Handles register
+    const handleRegister = ()=>{
         if(password !== repPassword){
             setErrorMsg("Passwords doesn't match");
         }else{
-            onClose();
+          //*Register common user
+          const registerUser = async ()=>{
+            try{
+              const res = await axios.post('http://localhost:5000/api/users/newc/',{
+                "name": name,
+                "lastName": lastName,
+                "username": username,
+                "password": password,
+                "age": age
+              });
+              console.log(res.data);
+              //*If the username exists, shows error
+              if(res.data.status === "error"){
+                setErrorMsg(res.data.message);
+              }else{
+                //*Make a login request
+                handleLogin();
+              }
+            }catch(e){
+              console.log("Error registering common user, registerModal.jsx: ", e);
+            }
+          }
+          registerUser();  
+          onClose();
         }
     }
 
@@ -47,12 +109,27 @@ function RegisterModal({onClose}) {
                 <input type="text" className='form-control' placeholder='Write your username here!' value={username} onChange={handleUsername}/>
             </div>
             <div className="col-12">
-                <label htmlFor="password" className="form-label">Password</label>
+                <label htmlFor="password" className="form-label">Password:</label>
                 <input type="password" className='form-control' placeholder='Write your password here!' value={password} onChange={handlePassword}/>
             </div>
             <div className='col-12'>
-                <label htmlFor="repPassword" className="form-label">Repeat your password</label>
+                <label htmlFor="repPassword" className="form-label">Repeat your password:</label>
                 <input type="password" className='form-control' placeholder='Please, repeat your password' value={repPassword} onChange={handleRepPassword} />
+            </div>
+
+            <div className="col-12">
+              <label htmlFor="name" className="form-label">Name:</label>
+              <input type="text" className="form-control" placeholder='Write down your name' value={name} onChange={handleName}/>
+            </div>
+
+            <div className="col-12">
+              <label htmlFor="lastName" className="form-label">Last name:</label>
+              <input type="text" className="form-control" placeholder='Write down your last name' value={lastName} onChange={handleLastName}/>
+            </div>
+
+            <div className="col-12">
+              <label htmlFor="age" className="form-label">Age:</label>
+              <input type="number" className="form-control" placeholder='Write down your age' value={age} onChange={handleAge}/>
             </div>
 
             {/* Displays error in case passwords doesnt match */}
@@ -63,7 +140,7 @@ function RegisterModal({onClose}) {
           <Button variant="danger" onClick={onClose}>
             Close
           </Button>
-          <Button variant="success" onClick={handleLogin}>
+          <Button variant="success" onClick={handleRegister}>
             Register
           </Button>
         </Modal.Footer>
